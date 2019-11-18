cd79134afe95da56f0c109a4030c46c6c1d3dfd7
Patrick Holody <patryk.holody@gmail.com>
2019-11-18 21:14:43 +0100

    prepared fetchapi

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.scss";
 import "../styles/main.scss";
-import SearchInput from "../atoms/SearchInput";
-import MovieDetails from "../organisms/MovieDetails";
+import SearchInput from "../components/atoms/SearchInput";
+import MovieDetails from "../components/organisms/MovieDetails";
+import MoviesAPI from "../api/MoviesAPI";
 
 const App: React.FC = () => {
+  const [movieList, setMovieList] = useState();
+
+  const fetchMovieList = async (name: string) => {
+    const newList = await MoviesAPI.getMoviesByTitle(name);
+    setMovieList(newList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">

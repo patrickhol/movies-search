650b80fa4f18c8685c65e76b9c127e8e27ccd96e
Patrick Holody <patryk.holody@gmail.com>
2019-11-22 22:51:10 +0100

    add movieType

---
+++ src/store/movie/movieTypes.ts
@@ -0,0 +1,26 @@
+import moviesInterface from "../../interfaces/moviesInterface.interface";
+
+export const SET_MOVIE_LIST = "SET_MOVIE_LIST";
+export const SET_MOVIE = "SET_MOVIE";
+export const SET_ERROR = "SET_ERROR";
+
+export interface MovieState {
+  movie?: moviesInterface;
+  movieList: moviesInterface[];
+  errorInfo: string;
+}
+interface AddMovie {
+  type: typeof SET_MOVIE;
+  payload: moviesInterface;
+}
+interface AddMovieList {
+  type: typeof SET_MOVIE_LIST;
+  payload: moviesInterface[];
+}
+
+interface AddError {
+  type: typeof SET_ERROR;
+  payload: string;
+}
+
+export type MovieActionTypes = AddMovie | AddMovieList | AddError;
